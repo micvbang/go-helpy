787f19c239f6aa4abad1946bf94d9b6557310abc
Michael Bang <mic@vbang.dk>
2022-06-04 13:10:32 +0200

    filey: stop using github.com/stretchr/testify/require

--- filey/exists_test.go
+++ filey/exists_test.go
@@ -6,51 +6,61 @@ import (
 	"testing"
 
 	"github.com/micvbang/go-helpy/filey"
-	"github.com/stretchr/testify/require"
 )
 
 func TestFileExists(t *testing.T) {
 	tests := map[string]struct {
-		fileName       string
-		expectedExists bool
-		isDir          bool
+		fileName string
+		expected bool
+		isDir    bool
 	}{
 		"file exists": {
-			fileName:       "file-1",
-			expectedExists: true,
+			fileName: "file-1",
+			expected: true,
 		},
 		"dir exists": {
-			fileName:       "dir-1",
-			expectedExists: true,
-			isDir:          true,
+			fileName: "dir-1",
+			expected: true,
+			isDir:    true,
 		},
 		"file doesn't exists": {
-			fileName:       "no-file-1",
-			expectedExists: false,
+			fileName: "no-file-1",
+			expected: false,
 		},
 	}
 
 	for n, test := range tests {
 		fileName := path.Join(os.TempDir(), test.fileName)
 
-		if test.expectedExists {
+		if test.expected {
 			if test.isDir {
 				err := os.Mkdir(fileName, os.ModePerm)
-				require.NoError(t, err)
+				if err != nil {
+					t.Fatalf("unexpected error when making dir %v: %v", fileName, err)
+				}
+
 			} else {
 				f, err := os.Create(fileName)
-				require.NoError(t, err)
+				if err != nil {
+					t.Fatalf("unexpected error when making file %v: %v", fileName, err)
+				}
+
 				f.Close()
 			}
 
 			defer func() {
 				err := os.Remove(fileName)
-				require.NoError(t, err)
+				if err != nil {
+					t.Fatalf("unexpected error when deleting %v: %v", fileName, err)
+				}
 			}()
 		}
 
 		t.Run(n, func(t *testing.T) {
-			require.Equal(t, test.expectedExists, filey.Exists(fileName))
+			got := filey.Exists(fileName)
+			if got != test.expected {
+				t.Errorf("expected %v, got %v", test.expected, got)
+			}
 		})
 
 	}
