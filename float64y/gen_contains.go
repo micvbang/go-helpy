2e6d9435caddef87726c2852e8dbedb4e7508783
Michael Bang <mic@vbang.dk>
2022-06-04 11:31:40 +0200

    helpy,gen: add generic Contains(), ContainsFunc() and use in generator

--- float64y/gen_contains.go
+++ float64y/gen_contains.go
@@ -2,13 +2,10 @@ package float64y
 
 // Code generated. DO NOT EDIT.
 
-// Contains returns true if vs contains v.
-func Contains(vs []float64, v float64) bool {
-	for _, c := range vs {
-		if c == v {
-			return true
-		}
-	}
+import "github.com/micvbang/go-helpy/slicey"
 
-	return false
+// Contains returns true if vs contains v.
+// NOTE: this function is deprecated. Use slicey.Contains instead.
+func Contains(vs []float64, v float64) bool {
+	return slicey.Contains(vs, v)
 }
---
+++ slicey/contains_test.go
@@ -0,0 +1,125 @@
+package slicey_test
+
+import (
+	"testing"
+
+	"github.com/micvbang/go-helpy/slicey"
+)
+
+// TestContains verifies that Contains returns true only when f returns true.
+func TestContains(t *testing.T) {
+	tests := map[string]struct {
+		haystack []int
+		needle   int
+		expected bool
+	}{
+		"nil haystack": {
+			haystack: nil,
+			expected: false,
+		},
+		"empty haystack": {
+			haystack: []int{},
+			expected: false,
+		},
+		"one element exists": {
+			haystack: []int{42},
+			needle:   42,
+			expected: true,
+		},
+		"one element, doesn't exist": {
+			haystack: []int{1337},
+			needle:   42,
+			expected: false,
+		},
+		"multiple elements, exists first": {
+			haystack: []int{42, 1337, -1500, 17, 71, 18821390293},
+			needle:   42,
+			expected: true,
+		},
+		"multiple elements, exists middle": {
+			haystack: []int{1337, -1500, 42, 17, 71, 18821390293},
+			needle:   42,
+			expected: true,
+		},
+		"multiple elements, exists last": {
+			haystack: []int{1337, -1500, 17, 71, 18821390293, 42},
+			needle:   42,
+			expected: true,
+		},
+		"multiple elements, doesn't exist": {
+			haystack: []int{-42, 1337, -1500, 17, 71, 18821390293},
+			needle:   42,
+			expected: false,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := slicey.Contains(test.haystack, test.needle)
+
+			if got != test.expected {
+				t.Errorf("expected exist %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+// TestContainsFunc verifies that Contains returns true only when f returns true.
+func TestContainsFunc(t *testing.T) {
+	tests := map[string]struct {
+		haystack []int
+		needle   int
+		expected bool
+	}{
+		"nil haystack": {
+			haystack: nil,
+			expected: false,
+		},
+		"empty haystack": {
+			haystack: []int{},
+			expected: false,
+		},
+		"one element exists": {
+			haystack: []int{42},
+			needle:   42,
+			expected: true,
+		},
+		"one element, doesn't exist": {
+			haystack: []int{1337},
+			needle:   42,
+			expected: false,
+		},
+		"multiple elements, exists first": {
+			haystack: []int{42, 1337, -1500, 17, 71, 18821390293},
+			needle:   42,
+			expected: true,
+		},
+		"multiple elements, exists middle": {
+			haystack: []int{1337, -1500, 42, 17, 71, 18821390293},
+			needle:   42,
+			expected: true,
+		},
+		"multiple elements, exists last": {
+			haystack: []int{1337, -1500, 17, 71, 18821390293, 42},
+			needle:   42,
+			expected: true,
+		},
+		"multiple elements, doesn't exist": {
+			haystack: []int{-42, 1337, -1500, 17, 71, 18821390293},
+			needle:   42,
+			expected: false,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := slicey.ContainsFunc(test.haystack, func(v int) bool {
+				return test.needle == v
+			})
+
+			if got != test.expected {
+				t.Errorf("expected exist %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
