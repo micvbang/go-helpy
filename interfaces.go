235ef484fe8238dc2ea0dc6a8669e6566ec5d7de
Michael Bang <mic@vbang.dk>
2022-06-04 11:09:01 +0200

    helpy: add Number and ComparableNumber interfaces

---
+++ interfaces.go
@@ -0,0 +1,9 @@
+package helpy
+
+type ComparableNumber interface {
+	int8 | int16 | int32 | int | int64 | uint8 | uint16 | uint32 | uint | uint64 | uintptr
+}
+
+type Number interface {
+	ComparableNumber | float32 | float64
+}
