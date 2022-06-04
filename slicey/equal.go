e43028eaaf7d577bd9dcdbfc73a44e24a1c28286
Michael Bang <mic@vbang.dk>
2022-06-04 12:09:43 +0200

    slicey: add Equal, EqualFunc

--- slicey/equal.go
+++ slicey/equal.go
@@ -15,3 +15,19 @@ func Equal[T comparable](vs1 []T, vs2 []T) bool {
 
 	return true
 }
+
+// EqualFunc returns true if vs1 and vs2 are of the same length and f returns
+// true for all pairs from vs1 and vs2 to be compared.
+func EqualFunc[T any](vs1 []T, vs2 []T, f func(v1 T, v2 T) bool) bool {
+	if len(vs1) != len(vs2) {
+		return false
+	}
+
+	for i := 0; i < len(vs1); i++ {
+		if !f(vs1[i], vs2[i]) {
+			return false
+		}
+	}
+
+	return true
+}
