d8e3d59c1a897d113ba466d0f05d2f9944fc3746
Michael Bang <mic@vbang.dk>
2022-08-02 13:16:33 +0200

    slicey/ContainsFunc: stop restricting input slice of comparable

--- slicey/contains.go
+++ slicey/contains.go
@@ -11,7 +11,7 @@ func Contains[T comparable](vs []T, needle T) bool {
 }
 
 // ContainsFunc returns true if f returns true for any element in vs.
-func ContainsFunc[T comparable](vs []T, f func(v T) bool) bool {
+func ContainsFunc[T any](vs []T, f func(v T) bool) bool {
 	for _, v := range vs {
 		if f(v) {
 			return true
